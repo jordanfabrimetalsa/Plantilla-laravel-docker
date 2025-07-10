a17fd505db9a26991da6b1b6917e334b379deb1a
jordanfabrimetalsa <jaguilera@fabrimetalsa.cl>
2025-07-10 15:18:33 -0400

    arreglando de talles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,33 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
-use App\Models\User;    
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        //gates
-        Gate::define('ver-admin', function(User $user){
-            return $user->rol == 'admin';
+        //gates en laravel
+        Gate::define('ver-admin', function (User $user){
+            return $user->rol === 'admin';
         });
 
-        Gate::define('ver-ventas', function(User $user){
+        Gate::define('ver-ventas', function (User $user){
             return in_array($user->rol, ['admin', 'cajero']);
         });
+
     }
 }
