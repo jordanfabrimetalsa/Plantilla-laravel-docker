f19e54ff6a6ea426bd0829203e80726d9b611524
aguileralabra <55728523+aguileralabra@users.noreply.github.com>
2025-07-14 01:59:13 -0400

    listo

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\Proveedores;
 use App\Http\Controllers\Reportes_productos;
 use App\Http\Controllers\Usuarios;
 use App\Http\Controllers\Ventas;
-use App\Http\Controllers\ProveedorController;
 use Illuminate\Support\Facades\Route;
 
 //crear un usuario admin, solo usar una vez
@@ -66,40 +65,11 @@ Route::prefix('productos')->middleware('auth', 'Checkrol:admin')->group(function
     Route::get('/cambiar-estado/{id}/{estado}', [Productos::class, 'estado'])->name('productos.estado');
 });
 
-    Route::prefix('cliente')->middleware('auth')->group(function(){
-        Route::get('/', [Clientes::class, 'index'])->name('cliente');
-        Route::get('/create', [Clientes::class, 'create'])->name('cliente.create');
-        Route::post('/store', [Clientes::class, 'store'])->name('cliente.store');
-        Route::get('/show/{id}', [Clientes::class, 'show'])->name('cliente.show');
-        Route::get('/edit/{id}', [Clientes::class, 'edit'])->name('cliente.edit');
-        Route::put('/update/{id}', [Clientes::class, 'update'])->name('cliente.update');
-        Route::delete('/destroy/{id}', [Clientes::class, 'destroy'])->name('cliente.destroy');
-        Route::put('/change-activo/{id}', [Clientes::class, 'changeActivo'])->name('cliente.changeActivo');
-    });
+Route::prefix('reportes_productos')->middleware('auth', 'Checkrol:admin')->group(function(){
+    Route::get('/', [Reportes_productos::class, 'index'])->name('reportes_productos');
+    Route::get('/falta-stock', [Reportes_productos::class, 'falta_stock'])->name('reportes_productos.falta_stock');
+});
 
-    Route::prefix('usuario')->middleware('auth')->group(function(){
-        Route::get('/', [Usuarios::class, 'index'])->name('usuario');
-        Route::get('/create', [Usuarios::class, 'create'])->name('usuario.create');
-        Route::get('/tbody', [Usuarios::class, 'tbody'])->name('usuario.tbody');
-        Route::get('/cambiar-estado/{id}/{estado}', [Usuarios::class, 'estado'])->name('usuario.estado');
-        Route::post('/store', [Usuarios::class, 'store'])->name('usuario.store');
-        Route::get('/show/{id}', [Usuarios::class, 'show'])->name('usuario.show');
-        Route::get('/edit/{id}', [Usuarios::class, 'edit'])->name('usuario.edit');
-        Route::put('/update/{id}', [Usuarios::class, 'update'])->name('usuario.update');
-        Route::get('/tbody', [Usuarios::class, 'tbody'])->name('usuario.tbody');
-        Route::get('/cambiar-estado/{id}/{estado}', [Usuarios::class, 'estado'])->name('usuario.estado');
-        Route::get('/cambiar-password/{id}/{password}', [Usuarios::class, 'cambio_password'])->name('usuario.password');
-    });
-
-    Route::prefix('proveedor')->middleware('auth')->group(function(){
-        Route::get('/', [ProveedorController::class, 'index'])->name('proveedor');
-        Route::get('/create', [ProveedorController::class, 'create'])->name('proveedor.create');
-        Route::post('/store', [ProveedorController::class, 'store'])->name('proveedor.store');
-        Route::get('/show/{id}', [ProveedorController::class, 'show'])->name('proveedor.show');
-        Route::get('/edit/{id}', [ProveedorController::class, 'edit'])->name('proveedor.edit');
-        Route::put('/update/{id}', [ProveedorController::class, 'update'])->name('proveedor.update');
-        Route::delete('/destroy/{id}', [ProveedorController::class, 'destroy'])->name('proveedor.destroy');
-    });
 
 Route::prefix('proveedores')->middleware('auth', 'Checkrol:admin')->group(function(){
     Route::get('/', [Proveedores::class, 'index'])->name('proveedores');
@@ -129,4 +99,5 @@ Route::prefix('compras')->middleware('auth', 'Checkrol:admin')->group(function()
     Route::get('/edit/{id}', [Compras::class, 'edit'])->name('compras.edit');
     Route::put('/update/{id}', [Compras::class, 'update'])->name('compras.update');
     Route::get('/show/{id}', [Compras::class, 'show'])->name('compras.show');
+    Route::delete('/destroy/{id}', [Compras::class, 'destroy'])->name('compras.destroy');
 });
